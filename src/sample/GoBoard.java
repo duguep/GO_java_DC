2bdda39fc5e947e965a44e65e419ede41fc5d949
rodolphe <rodolphe.dugueperoux@epitech.eu>
2017-11-21 12:07:32 +0000

    bla

---
+++ src/sample/GoBoard.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Translate;
+
+public class GoBoard extends Pane
+{
+    // rectangle that makes the background of the board
+    private Rectangle background;
+    // arrays for the lines that makeup the horizontal and vertical grid lines
+    private Line[] horizontal;
+    private Line[] vertical;
+    // arrays holding translate objects for the horizontal and vertical grid lines
+    private Translate[] horizontal_t;
+    private Translate[] vertical_t;
+    // arrays for the internal representation of the board and the pieces that are
+    // in place
+    private GoPiece[][] render;
+    // the current player who is playing and who is his opposition
+    private int current_player;
+    private int opposing;
+    // is the game currently in play
+    private boolean in_play;
+    // current scores of player 1 and player 2
+    private int player1_score;
+    private int player2_score;
+    // the width and height of a cell in the board
+    private double cell_width;
+    private double cell_height;
+    // 3x3 array that holds the pieces that surround a given piece
+    private int[][] surrounding;
+    // 3x3 array that determines if a reverse can be made in any direction
+    private boolean[][] can_reverse;
+
+    public GoBoard()
+    {
+
+    }
+
+}
